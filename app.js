2c2e4f838faa86a19191bcc42b12e3ea5eaeb454
Rayhan Yovi <yovihan@gmail.com>
2023-07-28 04:12:41 +0700

    menambahkan method Post untuk menambahkan komentar

--- app.js
+++ app.js
@@ -41,3 +41,43 @@ app.get("/video/:id", async (req, res) => {
 
   res.render("hehe.ejs", { video, products, comments });
 });
+
+app.post("/submit-comment", async (req, res) => {
+  const { username, comment, videoId } = req.body;
+
+  try {
+    // Create a new Comment document with the input data
+    const newComment = new Comment({
+      username: username,
+      comment: comment,
+      video: videoId, // Set the video field to the ObjectId of the associated video
+    });
+
+    // Save the newComment to the "comment_list" collection
+    const savedComment = await newComment.save();
+
+    console.log("Comment saved to database:", savedComment);
+
+    // Send a response containing a script that shows the alert
+    const alertScript = `
+      <script>
+        alert("Comment submitted successfully!");
+        window.location.href = "/"; // Redirect to the home page after the alert
+      </script>
+    `;
+
+    res.send(alertScript);
+  } catch (error) {
+    console.error("Error saving comment:", error);
+
+    // Send a response containing a script that shows the alert for the error
+    const alertScript = `
+      <script>
+        alert("An error occurred while saving the comment. Please try again later.");
+        window.location.href = "/"; // Redirect to the home page after the alert
+      </script>
+    `;
+
+    res.send(alertScript);
+  }
+});
--- models/comment.js
+++ models/comment.js
@@ -2,14 +2,10 @@ const mongoose = require("mongoose");
 
 const commentSchema = new mongoose.Schema(
   {
-    video_id: {
-      type: mongoose.Schema.Types.ObjectId,
-      default: mongoose.Types.ObjectId,
-      unique: true,
-    },
+    video: { type: mongoose.Schema.Types.ObjectId, ref: "Video" },
     username: { type: String, required: true },
-    created_at: { type: Number, required: true },
-    profile_pict: { type: Date, required: true },
+    created_at: { type: Number, required: false },
+    profile_pict: { type: Date, required: false },
     comment: { type: String, required: true },
   },
   { collection: "comment_list" }
